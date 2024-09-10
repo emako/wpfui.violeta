c6deb51f242ae813dc0789be68f744f038e2cb09
ema <mccoy39082@163.com>
2024-09-10 17:22:56 +0800

    change: add IsOnlyApplicationDispatcher option

--- src/Wpf.Ui.Violeta/Controls/ExceptionReport/ExceptionReport.cs
+++ src/Wpf.Ui.Violeta/Controls/ExceptionReport/ExceptionReport.cs
@@ -9,6 +9,12 @@ namespace Wpf.Ui.Violeta.Controls;
 
 public static class ExceptionReport
 {
+    /// <summary>
+    /// For safety only use `Application.Current.Dispatcher` for show dialog.
+    /// It means the temporary STA thread will be skipped.
+    /// </summary>
+    public static bool IsOnlyApplicationDispatcher { get; set; } = true;
+
     public static void HandleOnUnhandledException(this Application app)
     {
         app.DispatcherUnhandledException -= OnApplicationUnhandledException;
@@ -28,7 +34,14 @@ public static class ExceptionReport
         Window? owner = Application.Current.Windows.OfType<Window>().FirstOrDefault(window => window.IsActive)
             ?? Application.Current.MainWindow;
 
-        _ = Application.Current.Dispatcher.Invoke(() => _ = new ExceptionWindow(e, appName, appVersion) { Owner = owner }.ShowDialog());
+        Dispatcher dispatcher = Application.Current.Dispatcher;
+
+        if (!IsOnlyApplicationDispatcher)
+        {
+            dispatcher = owner?.Dispatcher ?? Application.Current.Dispatcher;
+        }
+
+        _ = dispatcher.Invoke(() => _ = new ExceptionWindow(e, appName, appVersion) { Owner = owner }.ShowDialog());
     }
 
     public static async Task ShowAsync(Exception e, string? appName = null, string? appVersion = null)
@@ -36,6 +49,13 @@ public static class ExceptionReport
         Window? owner = Application.Current.Windows.OfType<Window>().FirstOrDefault(window => window.IsActive)
             ?? Application.Current.MainWindow;
 
-        await Application.Current.Dispatcher.BeginInvoke(() => _ = new ExceptionWindow(e, appName, appVersion) { Owner = owner }.ShowDialog());
+        Dispatcher dispatcher = Application.Current.Dispatcher;
+
+        if (!IsOnlyApplicationDispatcher)
+        {
+            dispatcher = owner?.Dispatcher ?? Application.Current.Dispatcher;
+        }
+
+        await dispatcher.BeginInvoke(() => _ = new ExceptionWindow(e, appName, appVersion) { Owner = owner }.ShowDialog());
     }
 }
