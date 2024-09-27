adbc922d551973c40e0523ed28875c25492671f0
ema <mccoy39082@163.com>
2024-09-27 13:58:03 +0800

    feat: ManifestResourceProvider and so on

---
+++ src/Wpf.Ui.Violeta/Resources/ManifestResourceProvider.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Wpf.Ui.Violeta.Resources;
+
+public static class ManifestResourceProvider
+{
+    public static bool HasResource(string name, Assembly? assembly = null)
+    {
+        if (assembly == null)
+        {
+            StackTrace stackTrace = new(1);
+            StackFrame? stackFrame = stackTrace.GetFrame(0);
+            MethodBase? methodBase = stackFrame?.GetMethod()!;
+
+            assembly = methodBase?.DeclaringType?.Assembly!;
+        }
+
+        return assembly.GetManifestResourceInfo(name) != null;
+    }
+
+    public static Stream GetStream(string name, Assembly? assembly = null)
+    {
+        if (assembly == null)
+        {
+            StackTrace stackTrace = new(1);
+            StackFrame? stackFrame = stackTrace.GetFrame(0);
+            MethodBase? methodBase = stackFrame?.GetMethod()!;
+
+            assembly = methodBase?.DeclaringType?.Assembly!;
+        }
+
+        Stream stream = assembly.GetManifestResourceStream(name)!;
+        return stream;
+    }
+
+    public static string GetString(string name, Encoding? encoding = null, Assembly? assembly = null)
+    {
+        if (assembly == null)
+        {
+            StackTrace stackTrace = new(1);
+            StackFrame? stackFrame = stackTrace.GetFrame(0);
+            MethodBase? methodBase = stackFrame?.GetMethod()!;
+
+            assembly = methodBase?.DeclaringType?.Assembly!;
+        }
+
+        using Stream stream = GetStream(name, assembly);
+        using StreamReader streamReader = new(stream, encoding ?? Encoding.UTF8);
+        return streamReader.ReadToEnd();
+    }
+
+    public static byte[] GetBytes(string name, Assembly? assembly = null)
+    {
+        if (assembly == null)
+        {
+            StackTrace stackTrace = new(1);
+            StackFrame? stackFrame = stackTrace.GetFrame(0);
+            MethodBase? methodBase = stackFrame?.GetMethod()!;
+
+            assembly = methodBase?.DeclaringType?.Assembly!;
+        }
+
+        using Stream stream = GetStream(name, assembly);
+        using BinaryReader reader = new(stream);
+        return reader.ReadBytes((int)stream.Length);
+    }
+}
---
+++ src/Wpf.Ui.Violeta/Resources/ResourcesProvider.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.IO.Packaging;
+using System.Text;
+using System.Windows;
+using System.Windows.Resources;
+
+namespace Wpf.Ui.Violeta.Resources;
+
+public static class ResourcesProvider
+{
+    static ResourcesProvider()
+    {
+        if (!UriParser.IsKnownScheme("pack"))
+        {
+            _ = PackUriHelper.UriSchemePack;
+        }
+    }
+
+    public static bool HasResource(string uriString)
+    {
+        try
+        {
+            Uri uri = new(uriString);
+            StreamResourceInfo info = Application.GetResourceStream(uri);
+            using Stream? stream = info?.Stream;
+            _ = stream;
+            return true;
+        }
+        catch
+        {
+        }
+        return false;
+    }
+
+    public static Stream GetStream(string uriString)
+    {
+        Uri uri = new(uriString);
+        StreamResourceInfo info = Application.GetResourceStream(uri);
+        return info?.Stream!;
+    }
+
+    public static bool TryGetStream(string uriString, out Stream? stream)
+    {
+        try
+        {
+            stream = GetStream(uriString);
+            return true;
+        }
+        catch
+        {
+        }
+        stream = null;
+        return false;
+    }
+
+    public static Stream? TryFindStream(string uriString)
+    {
+        try
+        {
+            return GetStream(uriString);
+        }
+        catch
+        {
+        }
+        return null;
+    }
+
+    public static string GetString(string uriString, Encoding? encoding = null)
+    {
+        using Stream stream = GetStream(uriString);
+        using StreamReader streamReader = new(stream, encoding ?? Encoding.UTF8);
+        return streamReader.ReadToEnd();
+    }
+
+    public static byte[] GetBytes(string uriString)
+    {
+        using Stream stream = GetStream(uriString);
+        using BinaryReader streamReader = new(stream);
+        return streamReader.ReadBytes((int)stream.Length);
+    }
+}
