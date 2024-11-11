323e36aa44030b2296cc2bd93b1754cc474912d2
ema <mccoy39082@163.com>
2024-11-11 18:34:58 +0800

    feat: ApplicationDispatcher

---
+++ src/Wpf.Ui.Violeta/Threading/ApplicationDispatcher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+
+namespace Wpf.Ui.Violeta.Threading;
+
+public static class ApplicationDispatcher
+{
+    public static Window MainWindow => Application.Current.Dispatcher.Invoke(() => Application.Current.MainWindow);
+
+    public static void Invoke(Action callback, params object[] args)
+    {
+        _ = Application.Current?.Dispatcher.Invoke(callback, args);
+    }
+
+    public static void Invoke(Action<Window> callback)
+    {
+        _ = Application.Current?.Dispatcher.Invoke(callback, MainWindow);
+    }
+
+    public static T Invoke<T>(Func<T> action)
+    {
+        T t = default!;
+        Application.Current?.Dispatcher.Invoke(() => t = action());
+        return t;
+    }
+
+    public static void BeginInvoke(Action callback, params object[] args)
+    {
+        _ = Application.Current?.Dispatcher.BeginInvoke(callback, args);
+    }
+
+    public static void BeginInvoke(Action<Window> callback)
+    {
+        _ = Application.Current?.Dispatcher.BeginInvoke(callback, MainWindow);
+    }
+}
