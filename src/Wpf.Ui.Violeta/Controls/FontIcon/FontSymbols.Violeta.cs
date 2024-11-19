33143905da3c1d1aa30259aa69b92bbdcafcf36f
ema <mccoy39082@163.com>
2024-11-19 09:11:59 +0800

    feat: Violeta Fluent Icons

---
+++ src/Wpf.Ui.Violeta/Controls/FontIcon/FontSymbols.Violeta.cs
@@ -0,0 +1,13 @@
+﻿namespace Wpf.Ui.Violeta.Controls;
+
+/// <summary>
+/// Violeta Fluent Icons
+/// https://github.com/emako/fluentui-violeta-icons
+/// </summary>
+public sealed class FontSymbols
+{
+    public const string Edit = "\xe900";
+    public const string Empty = "\xe901";
+    public const string Extension = "\xe902";
+    public const string File = "\xe903";
+}
