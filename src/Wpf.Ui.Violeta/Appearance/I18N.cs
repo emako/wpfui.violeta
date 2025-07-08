4d6dc961f7b93c8e292a8d3a3b690c12051012e1
ema <mccoy39082@163.com>
2025-07-08 17:41:18 +0800

    feat: add I18N helper for resource localization

---
+++ src/Wpf.Ui.Violeta/Appearance/I18N.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using Wpf.Ui.Violeta.Resources.Localization;
+
+namespace Wpf.Ui.Violeta.Appearance;
+
+public static class I18N
+{
+    /// <summary>
+    /// Expose the getter and setter of internal resource languages
+    /// </summary>
+    public static CultureInfo Culture
+    {
+        get => SH.Culture;
+        set => SH.Culture = value;
+    }
+}
