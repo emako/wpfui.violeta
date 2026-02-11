2649cb6e438014e800d229a62a3658ed50dce709
ema <mccoy39082@163.com>
2026-02-12 00:15:36 +0800

    feat: integrate DependencyPropertyGenerator analyzer

---
+++ src/Wpf.Ui.Test/DependencyPropertyGenerate.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using Wpf.Ui.DependencyPropertyGenerator;
+
+namespace Wpf.Ui.Test;
+
+internal sealed partial class DependencyPropertyGenerate : DependencyObject
+{
+    [DependencyProperty(DefaultValue = 42, PropertyChanged = nameof(OnCountChanged))]
+    public int Count { get; set; }
+
+    private static void OnCountChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        // ...
+    }
+}
