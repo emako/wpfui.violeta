8def70bd2257b8205bbabc0d03e45f537bb77a0b
ema <mccoy39082@163.com>
2026-05-22 19:14:19 +0800

    Add AcrylicPanel and AcrylicBrushExtension

---
+++ src/Wpf.Ui.Violeta/Controls/AcrylicPanel/AcrylicPanel.cs
@@ -0,0 +1,192 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Wpf.Ui.Violeta.Controls;
+
+[TemplatePart(Name = PartBlurLayer, Type = typeof(FrameworkElement))]
+public class AcrylicPanel : ContentControl
+{
+    private const string PartBlurLayer = "PART_BlurLayer";
+
+    private FrameworkElement? _blurLayer;
+
+    static AcrylicPanel()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof(AcrylicPanel),
+            new FrameworkPropertyMetadata(typeof(AcrylicPanel)));
+    }
+
+    public AcrylicPanel()
+    {
+        Target ??= this;
+        Source ??= this;
+    }
+
+    public static readonly DependencyProperty TargetProperty =
+        DependencyProperty.Register(
+            nameof(Target),
+            typeof(FrameworkElement),
+            typeof(AcrylicPanel),
+            new PropertyMetadata(null, OnTargetOrSourceChanged));
+
+    public FrameworkElement? Target
+    {
+        get => (FrameworkElement?)GetValue(TargetProperty);
+        set => SetValue(TargetProperty, value);
+    }
+
+    public static readonly DependencyProperty SourceProperty =
+        DependencyProperty.Register(
+            nameof(Source),
+            typeof(FrameworkElement),
+            typeof(AcrylicPanel),
+            new PropertyMetadata(null, OnTargetOrSourceChanged));
+
+    public FrameworkElement? Source
+    {
+        get => (FrameworkElement?)GetValue(SourceProperty);
+        set => SetValue(SourceProperty, value);
+    }
+
+    public static readonly DependencyProperty CornerRadiusProperty =
+        DependencyProperty.Register(
+            nameof(CornerRadius),
+            typeof(CornerRadius),
+            typeof(AcrylicPanel),
+            new PropertyMetadata(new CornerRadius(0)));
+
+    public CornerRadius CornerRadius
+    {
+        get => (CornerRadius)GetValue(CornerRadiusProperty);
+        set => SetValue(CornerRadiusProperty, value);
+    }
+
+    public static readonly DependencyProperty AmountProperty =
+        DependencyProperty.Register(
+            nameof(Amount),
+            typeof(double),
+            typeof(AcrylicPanel),
+            new PropertyMetadata(24d));
+
+    public double Amount
+    {
+        get => (double)GetValue(AmountProperty);
+        set => SetValue(AmountProperty, value);
+    }
+
+    public static readonly DependencyProperty TintColorProperty =
+        DependencyProperty.Register(
+            nameof(TintColor),
+            typeof(Color),
+            typeof(AcrylicPanel),
+            new PropertyMetadata(Color.FromArgb(255, 30, 30, 30)));
+
+    public Color TintColor
+    {
+        get => (Color)GetValue(TintColorProperty);
+        set => SetValue(TintColorProperty, value);
+    }
+
+    public static readonly DependencyProperty TintOpacityProperty =
+        DependencyProperty.Register(
+            nameof(TintOpacity),
+            typeof(double),
+            typeof(AcrylicPanel),
+            new PropertyMetadata(0.52d));
+
+    public double TintOpacity
+    {
+        get => (double)GetValue(TintOpacityProperty);
+        set => SetValue(TintOpacityProperty, value);
+    }
+
+    public static readonly DependencyProperty NoiseOpacityProperty =
+        DependencyProperty.Register(
+            nameof(NoiseOpacity),
+            typeof(double),
+            typeof(AcrylicPanel),
+            new PropertyMetadata(0.03d));
+
+    public double NoiseOpacity
+    {
+        get => (double)GetValue(NoiseOpacityProperty);
+        set => SetValue(NoiseOpacityProperty, value);
+    }
+
+    public override void OnApplyTemplate()
+    {
+        DetachTrackedEvents();
+        base.OnApplyTemplate();
+
+        _blurLayer = GetTemplateChild(PartBlurLayer) as FrameworkElement;
+
+        AttachTrackedEvents();
+        UpdateBlurTransform();
+    }
+
+    private static void OnTargetOrSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is AcrylicPanel panel)
+        {
+            panel.DetachTrackedEvents();
+            panel.AttachTrackedEvents();
+            panel.UpdateBlurTransform();
+        }
+    }
+
+    private void AttachTrackedEvents()
+    {
+        LayoutUpdated += OnTrackedLayoutUpdated;
+
+        if (Target != null)
+        {
+            Target.LayoutUpdated += OnTrackedLayoutUpdated;
+        }
+
+        if (Source != null && Source != Target)
+        {
+            Source.LayoutUpdated += OnTrackedLayoutUpdated;
+        }
+    }
+
+    private void DetachTrackedEvents()
+    {
+        LayoutUpdated -= OnTrackedLayoutUpdated;
+
+        if (Target != null)
+        {
+            Target.LayoutUpdated -= OnTrackedLayoutUpdated;
+        }
+
+        if (Source != null && Source != Target)
+        {
+            Source.LayoutUpdated -= OnTrackedLayoutUpdated;
+        }
+    }
+
+    private void OnTrackedLayoutUpdated(object? sender, EventArgs e)
+    {
+        UpdateBlurTransform();
+    }
+
+    private void UpdateBlurTransform()
+    {
+        if (_blurLayer == null || Target == null || Source == null)
+        {
+            return;
+        }
+
+        try
+        {
+            Point sourceInTarget = Source.TransformToVisual(Target).Transform(new Point(0, 0));
+
+            _blurLayer.RenderTransform = new TranslateTransform(-sourceInTarget.X, -sourceInTarget.Y);
+        }
+        catch
+        {
+            _blurLayer.RenderTransform = Transform.Identity;
+        }
+    }
+}
---
+++ src/Wpf.Ui.Violeta/Markup/AcrylicBrushExtension.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+using Wpf.Ui.Violeta.Controls;
+
+namespace Wpf.Ui.Violeta.Markup;
+
+public class AcrylicBrushExtension : MarkupExtension
+{
+    public FrameworkElement? Target { get; set; }
+
+    public string? TargetName { get; set; }
+
+    public double? Amount { get; set; }
+
+    public Color? TintColor { get; set; }
+
+    public double? TintOpacity { get; set; }
+
+    public double? NoiseOpacity { get; set; }
+
+    public AcrylicBrushExtension()
+    {
+    }
+
+    public AcrylicBrushExtension(string target)
+    {
+        TargetName = target;
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        if (Target == null && !string.IsNullOrWhiteSpace(TargetName))
+        {
+            if (serviceProvider.GetService(typeof(IProvideValueTarget)) is IProvideValueTarget pvt)
+            {
+                if (pvt.TargetObject is FrameworkElement ownerElement)
+                {
+                    Target = ownerElement.FindName(TargetName) as FrameworkElement;
+                }
+                else if (pvt.TargetObject is FrameworkContentElement ownerContentElement)
+                {
+                    Target = ownerContentElement.FindName(TargetName) as FrameworkElement;
+                }
+            }
+        }
+
+        if (Target == null)
+        {
+            if (serviceProvider.GetService(typeof(IProvideValueTarget)) is IProvideValueTarget pvt)
+            {
+                Target = pvt.TargetObject as FrameworkElement;
+            }
+        }
+        else
+        {
+            TargetName = Target.Name;
+        }
+
+        if (Target is null)
+        {
+            return new SolidColorBrush(TintColor ?? Colors.Transparent);
+        }
+
+        return CreateAcrylicBrushInternal(Target);
+    }
+
+    public Brush CreateAcrylicBrush()
+    {
+        if (Target is null)
+        {
+            return new SolidColorBrush(TintColor ?? Colors.Transparent);
+        }
+
+        TargetName = Target.Name;
+        return CreateAcrylicBrushInternal(Target);
+    }
+
+    private Brush CreateAcrylicBrushInternal(FrameworkElement target)
+    {
+        var acrylicPanel = new AcrylicPanel();
+
+        if (Amount != null)
+        {
+            acrylicPanel.Amount = (double)Amount;
+        }
+
+        if (TintColor != null)
+        {
+            acrylicPanel.TintColor = (Color)TintColor;
+        }
+
+        if (TintOpacity != null)
+        {
+            acrylicPanel.TintOpacity = (double)TintOpacity;
+        }
+
+        if (NoiseOpacity != null)
+        {
+            acrylicPanel.NoiseOpacity = (double)NoiseOpacity;
+        }
+
+        acrylicPanel.SetBinding(AcrylicPanel.TargetProperty, new Binding { Source = target });
+        acrylicPanel.SetBinding(AcrylicPanel.SourceProperty, new Binding { Source = target });
+        acrylicPanel.SetBinding(FrameworkElement.WidthProperty, new Binding("ActualWidth") { Source = target });
+        acrylicPanel.SetBinding(FrameworkElement.HeightProperty, new Binding("ActualHeight") { Source = target });
+
+        var brush = new VisualBrush(acrylicPanel)
+        {
+            Stretch = Stretch.None,
+            AlignmentX = AlignmentX.Left,
+            AlignmentY = AlignmentY.Top,
+            ViewboxUnits = BrushMappingMode.Absolute,
+        };
+
+        return brush;
+    }
+}
