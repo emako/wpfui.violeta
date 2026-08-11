fda89a4fdaf4ce9ec45f2f2a84e691328346449b
ema <mccoy39082@163.com>
2026-08-11 11:50:39 +0800

    Add context menu placement demos and fix slide

--- src/Wpf.Ui.Violeta.Gallery/Pages/Navigation/MenuPage.xaml.cs
+++ src/Wpf.Ui.Violeta.Gallery/Pages/Navigation/MenuPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Windows;
+using System.Windows.Controls.Primitives;
 using Wpf.Ui.Violeta.Controls;
 
 namespace Wpf.Ui.Violeta.Gallery.Pages.Navigation;
@@ -7,5 +9,19 @@ public partial class MenuPage : Page
     public MenuPage()
     {
         InitializeComponent();
+    }
+
+    private void OnOpenContextMenuClick(object sender, RoutedEventArgs e)
+    {
+        if (sender is not FrameworkElement { ContextMenu: { } menu } host)
+        {
+            return;
+        }
+
+        menu.PlacementTarget = host;
+        menu.Placement = PlacementMode.Bottom;
+        menu.HorizontalOffset = 0;
+        menu.VerticalOffset = 1;
+        menu.IsOpen = true;
     }
 }
