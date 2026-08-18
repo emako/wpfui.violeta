010cf78b983e9b48789d0f5a93fa50c8bd11d927
ema <mccoy39082@163.com>
2026-08-18 19:27:53 +0800

    Remove obsolete Wpf.Ui test demo projects

--- src/Wpf.Ui.Test.NavigationView/Pages/HomeDemoPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace Wpf.Ui.Test.NavigationView.Pages;
-
-public partial class HomeDemoPage : Wpf.Ui.Violeta.Controls.Page
-{
-    public HomeDemoPage()
-    {
-        InitializeComponent();
-    }
-}
--- src/Wpf.Ui.Test.NavigationView/Pages/SettingsDemoPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace Wpf.Ui.Test.NavigationView.Pages;
-
-public partial class SettingsDemoPage : Wpf.Ui.Violeta.Controls.Page
-{
-    public SettingsDemoPage()
-    {
-        InitializeComponent();
-    }
-}
