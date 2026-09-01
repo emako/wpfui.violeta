5da2407415ac9caf601ed5d28ef5afb720d64b2b
ema <mccoy39082@163.com>
2026-09-01 20:38:17 +0800

    Add command support to custom ComboBoxItem

--- src/Wpf.Ui.Violeta/Controls/ComboBox/ComboBoxItem.cs
+++ src/Wpf.Ui.Violeta/Controls/ComboBox/ComboBoxItem.cs
@@ -1,11 +1,13 @@
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 
 namespace Wpf.Ui.Violeta.Controls;
 
 /// <summary>
-/// ComboBox item with optional <see cref="InputGestureText"/> (menu-style shortcut hint).
+/// ComboBox item with optional <see cref="InputGestureText"/> and <see cref="Command"/> support.
 /// </summary>
 public class ComboBoxItem : System.Windows.Controls.ComboBoxItem
 {
@@ -45,5 +47,51 @@ public class ComboBoxItem : System.Windows.Controls.ComboBoxItem
     {
         get => (string)GetValue(InputGestureTextProperty);
         set => SetValue(InputGestureTextProperty, value);
+    }
+
+    /// <summary>Identifies the <see cref="Command"/> dependency property.</summary>
+    public static readonly DependencyProperty CommandProperty = ButtonBase.CommandProperty.AddOwner(
+        typeof(ComboBoxItem));
+
+    /// <summary>
+    /// Gets or sets the command invoked when this item becomes selected.
+    /// </summary>
+    [Bindable(true)]
+    [Category("Action")]
+    public ICommand? Command
+    {
+        get => (ICommand?)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+    /// <summary>Identifies the <see cref="CommandParameter"/> dependency property.</summary>
+    public static readonly DependencyProperty CommandParameterProperty =
+        ButtonBase.CommandParameterProperty.AddOwner(typeof(ComboBoxItem));
+
+    /// <summary>
+    /// Gets or sets the parameter passed to <see cref="Command"/> when it is invoked.
+    /// </summary>
+    [Bindable(true)]
+    [Category("Action")]
+    public object? CommandParameter
+    {
+        get => GetValue(CommandParameterProperty);
+        set => SetValue(CommandParameterProperty, value);
+    }
+
+    protected override void OnSelected(RoutedEventArgs e)
+    {
+        base.OnSelected(e);
+        TryExecuteCommand();
+    }
+
+    private void TryExecuteCommand()
+    {
+        var parameter = CommandParameter;
+        var command = Command;
+        if (command?.CanExecute(parameter) == true)
+        {
+            command.Execute(parameter);
+        }
     }
 }
